16bb579e300f3d4f90e415c3c5c01753928d91d6
iamvengeance0 <readytorollbro@proton.me>
2024-09-03 23:21:56 +0200

    speed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // You can add interactivity with JavaScript here
 import { inject } from "@vercel/analytics"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 document.addEventListener("DOMContentLoaded", function () {
     // Example: Add smooth scrolling to anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
